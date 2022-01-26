f7e3c04f331da32379795d4713d149ac5ebfdb91
size0bit <size0bit@outlook.com>
2022-01-27 00:10:20 +0800

    test commit

--- chapter-2/src/main.rs
+++ chapter-2/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     loop {
         println!("猜一个数!");
         let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("无法读取行");
+        io::stdin().read_line(&mut guess).expect("无法读取行!");
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
