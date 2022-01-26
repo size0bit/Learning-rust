d623577d0a7332560108d2f5429aacc4a03eced2
size0bit <size0bit@outlook.com>
2022-01-26 23:49:33 +0800

    测试提交ââ

---
+++ chapter-2/src/main.rs
@@ -0,0 +1,27 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("猜数");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    //println!("神秘数字是：{}", secret_number);
+    loop {
+        println!("猜一个数");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("无法读取行");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("猜测的数是：{}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("太小了!"),
+            Ordering::Greater => println!("太大了"),
+            Ordering::Equal => {
+                println!("你赢了");
+                break;
+            }
+        }
+    }
+}
