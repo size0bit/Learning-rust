e9c208956cf5e8762ca13cd9007eea965a0a668c
size0bit <size0bit@outlook.com>
2022-01-27 00:05:26 +0800

    测试提交

--- chapter-2/src/main.rs
+++ chapter-2/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..101);
     //println!("神秘数字是：{}", secret_number);
     loop {
-        println!("猜一个数");
+        println!("猜一个数!");
         let mut guess = String::new();
         io::stdin().read_line(&mut guess).expect("无法读取行");
         let guess: u32 = match guess.trim().parse() {
